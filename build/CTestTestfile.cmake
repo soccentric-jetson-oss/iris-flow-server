96b4757f58cf6f4f6c4883aba23a3c15c29d5188
Sandesh Ghimire <sandesh@soccentric.com>
2026-08-05 19:00:23 -0700

    fix: .gitignore with proper line-separated patterns

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sandesh/jetson/build/iris-flow-server
-# Build directory: /home/sandesh/jetson/build/iris-flow-server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[iris-flow-server-test]=] "/home/sandesh/jetson/build/iris-flow-server/build/iris-flow-server-test")
-set_tests_properties([=[iris-flow-server-test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/sandesh/jetson/build/iris-flow-server/CMakeLists.txt;46;add_test;/home/sandesh/jetson/build/iris-flow-server/CMakeLists.txt;0;")
-subdirs("_deps/catch2-build")
